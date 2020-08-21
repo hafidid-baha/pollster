10b9c75cc0cbc48050a62812173a63a40845301e
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-21 13:34:11 +0100

    defining choise model

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
-class question(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('published date')
 
     def __str__(self):
         return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCAD)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerFiels(default=0)
+
+    def __str__(self):
+        return self.choise_text

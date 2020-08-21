daf71671fec2eaf68ae1208f8bad992e845d253d
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-21 13:46:15 +0100

    generats maigration files

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -9,9 +9,9 @@ class Question(models.Model):
         return self.question_text
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question,on_delete=models.CASCAD)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choise_text = models.CharField(max_length=200)
-    votes = models.IntegerFiels(default=0)
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choise_text

0376a54980ee9fee92ac4fe09cebe06ac6588346
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-21 14:37:53 +0100

    changing some admin titles

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Question,Choice
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Admin"
+admin.site.index_title = "Pollster Admin"
+
+
 admin.site.register(Question)
 admin.site.register(Choice)

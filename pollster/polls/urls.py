71cd2f261717a1c0145e76f82f796f911bbe4cec
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-21 17:17:34 +0100

    adding vote functionality

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -5,6 +5,7 @@ app_name = "polls"
 urlpatterns=[
     path('',views.index,name="index"),
     path('<int:question_id>/',views.details,name="detail"),
-    path('<int:question_id>/results/',views.results,name="result")
+    path('<int:question_id>/results/',views.results,name="result"),
+    path('<int:question_id>/vote/',views.vote,name="vote")
 ]
 
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Question,Choice
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
@@ -18,7 +21,16 @@ def results(request,question_id):
     question = Question.objects.get(pk=question_id)
     return render(request,'polls/results.html',{'question':question})
 
-
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/details.html',{ 'question':question })
+    else:
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse('polls:result',args=(question.id,)))
 
 
 

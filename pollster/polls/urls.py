116e5834c1ebf7091977e12aad7222fd6655e2eb
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-21 16:10:39 +0100

    add results url

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "polls"
 urlpatterns=[
     path('',views.index,name="index"),
-    path('<int:question_id>',views.details,name="detail")
+    path('<int:question_id>/',views.details,name="detail"),
+    path('<int:question_id>/results/',views.results,name="result")
 ]
 

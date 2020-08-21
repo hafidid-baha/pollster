2067fe1471f475ef5d7996d770f62e0b354a9b88
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-21 16:48:44 +0100

    create resuls page and adding some links

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -15,8 +15,10 @@ def details(request,question_id):
     return render(request,'polls/details.html',{'question':question})
 
 def results(request,question_id):
-    question = get_object_or_404(Question,pk=question.id)
+    question = Question.objects.get(pk=question_id)
     return render(request,'polls/results.html',{'question':question})
+
+
 
 
 

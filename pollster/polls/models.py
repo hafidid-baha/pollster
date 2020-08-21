7c2cd4288abfbd7b88670517a3253ad3f582e7f6
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-21 13:30:04 +0100

    create questions model

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('published date')
+
+    def __str__(self):
+        return self.question_text
